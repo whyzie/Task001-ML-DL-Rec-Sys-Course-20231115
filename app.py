f465dab23bb27ab9e6a81777d1a9c70be8b7148e
Wahyu Ardhitama <97485455+whyzie@users.noreply.github.com>
2024-02-17 20:24:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,8 +101,11 @@ def main():
 						rec_num_sub = row[1][4]
 
 					# Display the recommendations using the RESULT_TEMP template
-					for recommendation in recommendations:
-						st.markdown(RESULT_TEMP.format(*recommendation), unsafe_allow_html=True)
+						for recommendation in recommendations:
+						# st.write("Title",rec_title,)
+						# stc.html(RESULT_TEMP.format(rec_title,rec_score,rec_url,rec_url,rec_num_sub),height=350)
+						st.markdown(RESULT_TEMP.format(rec_title,rec_score,rec_url,rec_url,rec_num_sub),height=350, unsafe_allow_html=True)
+						
 				except:
 					results= "Not Found"
 					st.warning(results)
